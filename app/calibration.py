11b92a55fbd46730f70c66ebb68869cc8961075f
Tadas Danielius <tadasda@lt.ibm.com>
2017-02-07 18:08:25 +0200

    working on report

--- app/calibration.py
+++ app/calibration.py
@@ -4,16 +4,21 @@ import glob
 import _pickle as pickle
 
 class Camera_calibrator:
-    def calibrate(self, img):
+    """ Camera calibration class is responsibole for calibrate and udistort images """
+    def undistort(self, img):
+        """ This method undistorts the image using cv2 library API """
         img_size = (img.shape[1], img.shape[0])
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, img_size,None,None)
         undist = cv2.undistort(img, mtx, dist, None, mtx)
         return undist
 
     def initialize(self, filename='calibration.pkl', corners=(9,6), cal_image_loc='camera_cal/*.jpg'):
+        """ Initialize calibrator. It will try to load calibration.pkl first, if not found then use provided images to detect corners and and calculate points """
         try:
+            # try to load it from file
             self.load_calibration_points(filename)
         except:
+            # in case file not found then calibrate and save
             _, _ = self.detect_corners(corners=corners, cal_image_loc=cal_image_loc)
             self.save_calibration_points(filename=filename)
 
--- app/warp.py
+++ app/warp.py
@@ -3,27 +3,8 @@ import cv2
 
 OFFSET = 250
 
-PERSPECTIVE_SRC = np.float32([
-                    (132, 703),
-                    (540, 466),
-                    (740, 466),
-                    (1147, 703)])
-
-PERSPECTIVE_DST = np.float32([
-                    (PERSPECTIVE_SRC[0][0] + OFFSET, 720),
-                    (PERSPECTIVE_SRC[0][0] + OFFSET, 0),
-                    (PERSPECTIVE_SRC[-1][0] - OFFSET, 0),
-                    (PERSPECTIVE_SRC[-1][0] - OFFSET, 720)])
-
-def get_warp_src(img):
+def get_warp_src(image):
     """ Source matrix """
-    img_size = (img.shape[1], img.shape[0])
-    image = img
-    src = np.float32(
-        [[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],
-         [((img_size[0] / 6) - 10), img_size[1]],
-         [(img_size[0] * 5 / 6) + 60, img_size[1]],
-         [(img_size[0] / 2 + 55), img_size[1] / 2 + 100]])
     src = np.float32([
         [image.shape[1]*0.4475, image.shape[0]*0.65],
         [image.shape[1]*0.5525, image.shape[0]*0.65],
@@ -32,21 +13,13 @@ def get_warp_src(img):
     ])
     return src
 
-def get_warp_dst(img):
+def get_warp_dst(image):
     """ Destination matrix """
-    img_size = (img.shape[1], img.shape[0])
-    image = img
-    dst = np.float32(
-        [[(img_size[0] / 4), 0],
-         [(img_size[0] / 4), img_size[1]],
-         [(img_size[0] * 3 / 4), img_size[1]],
-         [(img_size[0] * 3 / 4), 0]])
-         
     dst = np.float32([
-        [image.shape[1]*0.2,image.shape[0]*0.025],
-        [image.shape[1]*0.8,image.shape[0]*0.025],
-        [image.shape[1]*0.2,image.shape[0]*0.975],
-        [image.shape[1]*0.8,image.shape[0]*0.975],
+        [image.shape[1]*0.2, image.shape[0]*0.025],
+        [image.shape[1]*0.8, image.shape[0]*0.025],
+        [image.shape[1]*0.2, image.shape[0]*0.975],
+        [image.shape[1]*0.8, image.shape[0]*0.975],
     ])
     return dst
 
@@ -56,8 +29,8 @@ def warp(img, src=None, dst=None):
     img_size = (img.shape[1], img.shape[0])
     src = src if src is not None else get_warp_src(img)
     dst = dst if dst is not None else get_warp_dst(img)
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    Mtx = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, Mtx, img_size, flags=cv2.INTER_LINEAR)
     return warped
 
 def unwarp(img):
